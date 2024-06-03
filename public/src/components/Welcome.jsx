933cdf2e201caa017b727ab457ea651d9bdff826
Aditya Gupta <aditya.gupta3182003@gmail.com>
2024-06-03 14:09:52 +0530

    pub

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome() {
+
+  const [userName, setUserName] = useState("");
+  
+  useEffect( () => {
+    async function fetchMyAPI() {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem('chat-app-user')//process.env.REACT_APP_LOCALHOST_KEY
+      ).username
+    );
+  }
+  fetchMyAPI();
+
+  }, []);
+  return (
+    <Container>
+      <img src={Robot} alt="" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start Messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
